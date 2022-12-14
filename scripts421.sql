2059e02ed16bc4b51ab41fec780de14492eeb9ca
danya1705 <danya17@gmail.com>
2022-12-14 19:20:05 +0300

    hw 3.4

--- scripts421.sql
+++ scripts421.sql
@@ -1,4 +1,5 @@
 ALTER TABLE student ADD CONSTRAINT age_constraint CHECK (age > 15);
-ALTER TABLE student ADD PRIMARY KEY (name);
+ALTER TABLE student ADD CONSTRAINT name_unique UNIQUE (name);
+ALTER TABLE student ALTER COLUMN name SET NOT NULL;
 ALTER TABLE faculty ADD CONSTRAINT name_color_constraint UNIQUE (name, color);
 ALTER TABLE student ALTER age SET DEFAULT 20;
